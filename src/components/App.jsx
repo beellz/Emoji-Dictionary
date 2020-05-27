fb7c0e7b64bb8d4233716f2d43bbd3e46719e502
beellz <belal20795@gmail.com>
2020-05-27 22:28:55 +0530

    basic without map function

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Entry from "./entry";
+import emojipedia from "../emojipedia"
+
+
+function createCard (emojipedia) {
+  
+}
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+    
+    
+    {/* <Entry 
+    name={emojipedia[0].name}
+    emoji={emojipedia[0].emoji}
+    meaning={emojipedia[0].meaning}
+    />
+       */}
+    </div>
+  );
+}
+
+export default App;
