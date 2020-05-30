801c596fbad204837e96c46ee0ebb57ca43228e9
beellz <belal20795@gmail.com>
2020-05-30 23:31:17 +0530

    adding website link to guthub

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Emoji-Dictionary
+
+You can view this website on : https://beellz.github.io/Emoji-Dictionary/
---
+++ build/precache-manifest.f6194dd33b90cfe7afa8361dc36283c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168f211c5bc8c845c17f8f7f0bff52f6",
+    "url": "/Emoji-Dictionary/index.html"
+  },
+  {
+    "revision": "5acb71f596532c1a6a0a",
+    "url": "/Emoji-Dictionary/static/js/2.f53d1ad4.chunk.js"
+  },
+  {
+    "revision": "6c6aa17b0986377cb4eb",
+    "url": "/Emoji-Dictionary/static/js/main.41254084.chunk.js"
+  },
+  {
+    "revision": "96dea96d3343329e54c0",
+    "url": "/Emoji-Dictionary/static/js/runtime-main.7e889104.js"
+  }
+]);
